722a694ae0014a56e0b7f1c12eaa2af397f12659
zhangsiming <zhangsiming@yy.com>
2021-05-07 21:46:49 +0800

    堆排序完事

--- jsMethod/call_apply.js
+++ jsMethod/call_apply.js
@@ -1,7 +1,7 @@
 function callFn () {
   let fn = this
-  const [ obj, args ] = [...arguments]
+  const [ obj, ...args ] = [...arguments]
   obj.fn = fn
-  obj.fn(arg1, args)
+  obj.fn(arg1, ...args)
   delete obj.fn
 }
--- jsMethod/paixu/topNxian.js
+++ jsMethod/paixu/topNxian.js
@@ -18,4 +18,36 @@ for (let i = 0; i < N; i++) {
 // 初次建立size = 10的小顶堆以这十亿个整数中前10个数
 for (let i = len / 2 - 1; i >= 0; i++) {
   // 每一个元素堆调整
+  minHeap(i)
+}
+// 现在arr就是小根堆 堆顶最小 这时候 8外的数和这个堆顶比  比堆顶小的话 直接pass 比堆顶大的话 和堆顶互换 然后进行堆调整 之后在循环比较
+for (let i = N; i < LEN; i++) {
+  if (arr[i]>arr[0]) {
+    arr[0] = arr[i]
+    minHeap(0)
+  }
+}
+console.log('一亿里面前十个元素', arr)
+/**
+ * i节点为根及子树是一个小堆
+ * @param i
+ */
+// 堆调整的筛选
+function minHeap(i) {
+  let left = 2*i-1
+  let right = 2*i+1
+  let small = i
+  if(left<heapSize && arr[left] < arr[small]) {
+    small = left
+  }
+  if(right<heapSize && arr[right] < arr[small]) {
+    small = right
+  }
+  let t // 存交换的暂时参数
+  if (small !== i) {
+    t = arr[small]
+    arr[small] = arr[i]
+    arr[i] = t
+    minHeap(small)
+  }
 }
