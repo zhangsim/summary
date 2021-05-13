71e01b1a0819922df4cc7c2b62a0737773276d25
zhangsiming <zhangsiming@yy.com>
2021-05-13 16:01:26 +0800

    reduce

--- jsMethod/实现数组的reduce方法.js
+++ jsMethod/实现数组的reduce方法.js
@@ -10,4 +10,8 @@ console.log(aaa)
 
 // reduce(callback(累加器,当前索引的元素,当前索引,调用的数据), 初始累加器（如果没有累加器是第一个元素,当前索引的元素从第二个元素开始）)
 
-Array.prototype
+Array.prototype.reducenew = function (fn,initValue) {
+  var arr = this
+  var currentIndex = initValue ? 0 : 1
+  var len = arr.length
+}
