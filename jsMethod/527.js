e31e67aeeb9099d973ef8c5cc4617867b5c488db
zhangsiming <zhangsiming@yy.com>
2021-05-28 18:21:18 +0800

    reduce

---
+++ jsMethod/527.js
@@ -0,0 +1,16 @@
+// js事件冒泡和事件捕获 e.target/e.currentTarget区别
+// promise实现，promise.all实现，都没返回数据(数据返回为空)怎么办
+// vue中的key
+// 组件间的传值方式有哪些
+// diff算法
+// 双向数据绑定原理，怎么收集依赖，监听到数据变化，更新的
+// 组件中的data为什么是函数
+// 介绍proxy 多少种拦截，说一两种
+// Generator，node相关的用过吗：
+// generator可以让某一个函数执行某个动作之后暂时停止，之后 再次触发，再次执行
+// watch，computed区别
+// vue开发公众号/微信h5页面遇到过什么坑，怎么解决的
+// vue的生命周期 ，created什么时候触发的，vue开发的优点
+// vue-router hash,history原理怎么实现的，看过源码吗，路由跳转的方式，区别
+// vue里引入element-ui这种的DOM挂载是怎么实现的
+// 父子组件生命周期的执行顺序
